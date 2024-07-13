399b0abab431a2d1879801b55d71047f6a71b4ff
dattgt <trangiathanhdat02092002@gmail.com>
2024-07-13 12:55:57 +0700

    fix User View

--- src/sections/user/user-table-row.jsx
+++ src/sections/user/user-table-row.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
-
 import Stack from '@mui/material/Stack';
 import Popover from '@mui/material/Popover';
 import TableRow from '@mui/material/TableRow';
@@ -10,49 +9,22 @@ import MenuItem from '@mui/material/MenuItem';
 import TableCell from '@mui/material/TableCell';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-
 import Label from 'src/components/label';
 import Iconify from 'src/components/iconify';
-
 import InfoModal from './user-modal';
 import DelModal from './user-del-modal';
 import EditModal from './user-edit-modal';
 
-// ----------------------------------------------------------------------
-
-export default function UserTableRow({
-  id,
-  selected,
-  fullName,
-  role,
-  email,
-  username,
-  status,
-  handleClick,
-  roleId,
-  password,
-  onDelete,
-  onUpdate,
-  gender,
-  counterId,
-  counterNumber,
-  phoneNumber
-}) {
-  console.log("id",id)
-
+export default function UserTableRow({ userData, selected, handleClick, onDelete, onUpdate }) {
   const [open, setOpen] = useState(null);
   const [showDel, setShowDel] = useState(false);
+  const [showEd, setShowEd] = useState(false);
+  const [show, setShow] = useState(false);
 
   const handleCloseDel = () => setShowDel(false);
   const handleShowDel = () => setShowDel(true);
-
-  const [showEd, setShowEd] = useState(false);
-
   const handleCloseEd = () => setShowEd(false);
   const handleShowEd = () => setShowEd(true);
-
-  const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -70,28 +42,41 @@ export default function UserTableRow({
         <TableCell padding="checkbox">
           <Checkbox disableRipple checked={selected} onChange={handleClick} />
         </TableCell>
-
         <TableCell component="th" scope="row" padding="none">
           <Stack direction="row" alignItems="center" spacing={2}>
             <Typography variant="subtitle2" noWrap>
-              {fullName}
+              {userData.username}
             </Typography>
           </Stack>
         </TableCell>
-
-      
-
-        <TableCell>{email}</TableCell>
-
+        <TableCell>{userData.email}</TableCell>
+        <TableCell>{userData.counterNumber}</TableCell>
         <TableCell>
-          <Label color={(role === 'Admin' && 'primary') || (role === 'Staff' && 'secondary') || 'success'}>{role}</Label>
+          <Label
+            color={
+              (userData.roleName === 'Admin' && 'primary') ||
+              (userData.roleName === 'Manager' && 'secondary') ||
+              (userData.roleName === 'Staff' && 'info') ||
+              'success'
+            }
+          >
+            {userData.roleName}
+          </Label>
         </TableCell>
-
+        <TableCell>
+          <Label color={userData.status ? 'success' : 'error'}>
+            {userData.status ? 'Active' : 'Inactive'}
+          </Label>
+        </TableCell>
         <TableCell align="right">
-        <Button variant="outline-primary" onClick={handleShow}>More Info</Button>
-          <IconButton onClick={handleOpenMenu}>
-            <Iconify icon="eva:more-vertical-fill" />
-          </IconButton>
+          <Button variant="outline-primary" onClick={handleShow}>
+            More Info
+          </Button>
+          {userData.roleName !== 'Admin' && (
+            <IconButton onClick={handleOpenMenu}>
+              <Iconify icon="eva:more-vertical-fill" />
+            </IconButton>
+          )}
         </TableCell>
       </TableRow>
 
@@ -105,38 +90,58 @@ export default function UserTableRow({
           sx: { width: 140 },
         }}
       >
-        <MenuItem onClick={() => { handleCloseMenu(); handleShowEd(); }}>
+        <MenuItem
+          onClick={() => {
+            handleCloseMenu();
+            handleShowEd();
+          }}
+        >
           <Iconify icon="eva:edit-fill" sx={{ mr: 2 }} />
           Edit
         </MenuItem>
 
-        <MenuItem onClick={() => { handleCloseMenu(); handleShowDel(); }} sx={{ color: 'error.main' }}>
+        <MenuItem
+          onClick={() => {
+            handleCloseMenu();
+            handleShowDel();
+          }}
+          sx={{ color: 'error.main' }}
+        >
           <Iconify icon="eva:trash-2-outline" sx={{ mr: 2 }} />
           Delete
         </MenuItem>
       </Popover>
 
-      <DelModal show={showDel} handleClose={handleCloseDel} fullName={fullName} roleId={roleId}  email={email} password={password} onDelete={onDelete} role={role}/>
+      <DelModal
+        show={showDel}
+        handleClose={handleCloseDel}
+        userData={userData}
+        onDelete={onDelete}
+      />
 
-      <InfoModal show={show} handleClose={handleClose} fullName={fullName} roleId={roleId}  email={email} password={password} role={role} />
-
-      <EditModal show={showEd} handleClose={handleCloseEd}  fullName={fullName} roleId={roleId} email={email} password={password} role={role} gender={gender}  onUpdate={(updatedData) => onUpdate(id, updatedData)} />
+      <InfoModal show={show} handleClose={handleClose} userData={userData} />
+      
+      <EditModal
+        show={showEd}
+        handleClose={handleCloseEd}
+        userData={userData}
+        onUpdate={onUpdate}
+      />
     </>
   );
 }
 
 UserTableRow.propTypes = {
-  id: PropTypes.any,
-  roleId: PropTypes.any,
-  handleClick: PropTypes.func,
-  email: PropTypes.any,
-  role: PropTypes.string,
-  fullName: PropTypes.any,
-  username: PropTypes.any,
-  status: PropTypes.bool,
-  selected: PropTypes.any,
-  password: PropTypes.any,
-  onDelete: PropTypes.func,
-  onUpdate: PropTypes.func,
-  gender: PropTypes.string
+  userData: PropTypes.shape({
+    userId: PropTypes.string,
+    username: PropTypes.string,
+    email: PropTypes.string,
+    counterNumber: PropTypes.number,
+    roleName: PropTypes.string,
+    status: PropTypes.bool,
+  }).isRequired,
+  selected: PropTypes.bool,
+  handleClick: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
--- src/sections/user/view/user-view.jsx
+++ src/sections/user/view/user-view.jsx
@@ -12,8 +12,6 @@ import Typography from '@mui/material/Typography';
 import TableContainer from '@mui/material/TableContainer';
 import TablePagination from '@mui/material/TablePagination';
 
-// import { fetchAllUsers } from "src/_mock/user";
-
 import Iconify from 'src/components/iconify';
 import Scrollbar from 'src/components/scrollbar';
 
@@ -40,7 +38,6 @@ export default function UserPage() {
   const [selected, setSelected] = useState([]);
   const [orderBy, setOrderBy] = useState('name');
   const [filterName, setFilterName] = useState('');
-  console.log("filterName",filterName)
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
   useEffect(() => {
@@ -49,22 +46,17 @@ export default function UserPage() {
 
   const getUser = async () => {
     const res = await axios.get("http://localhost:5188/api/User/GetUsers");
-    console.log("res",res);
     setUserList(res.data);
   }
-
-  console.log(userList)
 
   const deleteUser = async (id) => {
     try {
       await axios.delete(`http://localhost:5188/api/User/DeleteUser/${id}`);
       setUserList(userList.filter((item) => item.id !== id));
-      toast.success('Delete user successful !', {
+      toast.success('Delete user successful!', {
         position: "bottom-right",
         theme: "colored",
       });
-
-      console.log('Delete successful');
     } catch (error) {
       console.error('There was an error:', error);
     }
@@ -73,9 +65,8 @@ export default function UserPage() {
   const createUser = async (newItem) => {
     try {
       const response = await axios.post('http://localhost:5188/api/User/AddUser', newItem);
-      // setUserList([...userList, response.data]);
       handleClose();
-      toast.success('Create user successful !', {
+      toast.success('Create user successful!', {
         position: "bottom-right",
         theme: "colored",
       });
@@ -85,19 +76,17 @@ export default function UserPage() {
     }
   }
 
-
   const updateUser = async (id, updatedData) => {
     try {
       const response = await axios.put(`http://localhost:5188/api/User/UpdateUser/${id}`, updatedData);
       const updatedUser = response.data;
       setUserList(prevData =>
-        prevData.map(item => (item.id === id ? updatedUser : item))
+        prevData.map(item => (item.userId === id ? updatedUser : item))
       );
-      toast.success('Update user successful !', {
+      toast.success('Update user successful!', {
         position: "bottom-right",
         theme: "colored",
       });
-  
       return updatedUser;
     } catch (error) {
       console.error('Error updating user:', error);
@@ -113,19 +102,19 @@ export default function UserPage() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = userList.map((user) => user.name);
+      const newSelecteds = userList.map((user) => user.username);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, username) => {
+    const selectedIndex = selected.indexOf(username);
     let newSelected = [];
 
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, username);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -166,7 +155,6 @@ export default function UserPage() {
     <Container>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">Users</Typography>
-
         <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleShow}>
           New User
         </Button>
@@ -193,9 +181,10 @@ export default function UserPage() {
                 headLabel={[
                   { id: 'username', label: 'User Name' },
                   { id: 'email', label: 'Email' },
-                  { id: 'role', label: 'Role' },
+                  { id: 'counterNumber', label: 'Counter Number' },
+                  { id: 'roleName', label: 'Role Name' },
+                  { id: 'status', label: 'Status' },
                   { id: '' },
-
                 ]}
               />
               <TableBody>
@@ -204,15 +193,11 @@ export default function UserPage() {
                   .map((user) => (
                     <UserTableRow
                       key={user.userId}
-                      id={user.userId}
-                      fullName={user.username}
-                      email={user.email}
-                      role={user.roleName}
-                      selected={selected.indexOf(user.id) !== -1}
-                      handleClick={(event) => handleClick(event, user.id)}
-                      onDelete={() => deleteUser(user.userId)} 
+                      userData={user}
+                      selected={selected.indexOf(user.username) !== -1}
+                      handleClick={(event) => handleClick(event, user.username)}
+                      onDelete={() => deleteUser(user.userId)}
                       onUpdate={updateUser}
-                      gender={user.gender}
                     />
                   ))}
 
