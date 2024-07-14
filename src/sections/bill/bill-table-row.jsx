0e697ae69238b1446a7a5f03747422f88339f6dd
dattgt <trangiathanhdat02092002@gmail.com>
2024-07-14 22:24:29 +0700

    call API for Bill View

--- src/sections/bill/bill-table-row.jsx
+++ src/sections/bill/bill-table-row.jsx
@@ -1,27 +1,18 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import Popover from '@mui/material/Popover';
 import TableRow from '@mui/material/TableRow';
 import Checkbox from '@mui/material/Checkbox';
 import MenuItem from '@mui/material/MenuItem';
 import TableCell from '@mui/material/TableCell';
 import IconButton from '@mui/material/IconButton';
-
+import Button from '@mui/material/Button';
 import Iconify from 'src/components/iconify';
+import BillDetailsDialog from './bill-details-dialog';
 
-// ----------------------------------------------------------------------
-
-export default function UserTableRow({
-  selected,
-  billId,
-  customerId,
-  staffId,
-  saleDate,
-  totalAmount,
-  handleClick,
-}) {
+export default function BillTableRow({ selected, bill, handleClick }) {
   const [open, setOpen] = useState(null);
+  const [showDetails, setShowDetails] = useState(false);
 
   const handleOpenMenu = (event) => {
     setOpen(event.currentTarget);
@@ -31,31 +22,38 @@ export default function UserTableRow({
     setOpen(null);
   };
 
+  const handleDetailsClose = () => {
+    setShowDetails(false);
+  };
+
+  const handleDetailsShow = () => {
+    setShowDetails(true);
+  };
+
   return (
     <>
       <TableRow hover tabIndex={-1} role="checkbox" selected={selected}>
         <TableCell padding="checkbox">
           <Checkbox disableRipple checked={selected} onChange={handleClick} />
         </TableCell>
-
-        <TableCell>{billId}</TableCell>
-
-        <TableCell>{customerId}</TableCell>
-
-        <TableCell>{staffId}</TableCell>
-
-        <TableCell>{totalAmount}</TableCell>
-
-        <TableCell>{saleDate}</TableCell>
-
+        <TableCell>{bill.staffName}</TableCell>
+        <TableCell>{bill.customerName}</TableCell>
+        <TableCell>{bill.totalAmount}</TableCell>
+        <TableCell>{bill.totalDiscount}</TableCell>
+        <TableCell>{new Date(bill.saleDate).toLocaleString()}</TableCell>
+        <TableCell>{bill.items.map(item => item.name).join(', ')}</TableCell>
+        <TableCell>{bill.finalAmount}</TableCell>
         <TableCell align="right">
-          <IconButton onClick={handleOpenMenu}>
+          <Button variant="outlined" onClick={handleDetailsShow}>
+            More Info
+          </Button>
+          {/* <IconButton onClick={handleOpenMenu}>
             <Iconify icon="eva:more-vertical-fill" />
-          </IconButton>
+          </IconButton> */}
         </TableCell>
       </TableRow>
 
-      <Popover
+      {/* <Popover
         open={!!open}
         anchorEl={open}
         onClose={handleCloseMenu}
@@ -74,17 +72,32 @@ export default function UserTableRow({
           <Iconify icon="eva:trash-2-outline" sx={{ mr: 2 }} />
           Delete
         </MenuItem>
-      </Popover>
+      </Popover> */}
+
+      <BillDetailsDialog
+        show={showDetails}
+        handleClose={handleDetailsClose}
+        bill={bill}
+      />
     </>
   );
 }
 
-UserTableRow.propTypes = {
-  billId: PropTypes.number,
-  customerId: PropTypes.number,
-  staffId: PropTypes.number,
-  totalAmount: PropTypes.number,
-  saleDate: PropTypes.instanceOf(Date),
-  handleClick: PropTypes.func,
-  selected: PropTypes.any,
+BillTableRow.propTypes = {
+  bill: PropTypes.shape({
+    billId: PropTypes.string.isRequired,
+    customerName: PropTypes.string.isRequired,
+    staffName: PropTypes.string.isRequired,
+    totalAmount: PropTypes.number.isRequired,
+    totalDiscount: PropTypes.number.isRequired,
+    saleDate: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    finalAmount: PropTypes.number.isRequired,
+  }).isRequired,
+  selected: PropTypes.bool,
+  handleClick: PropTypes.func.isRequired,
 };
